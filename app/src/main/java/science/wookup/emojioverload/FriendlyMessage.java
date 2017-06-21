5d0188900e4030f9eac573f779e1d89b17512981
Varun Dixit <wookupmaker@gmail.com>
2017-06-21 12:42:22 +0530

    Initial commit

---
+++ app/src/main/java/science/wookup/emojioverload/FriendlyMessage.java
@@ -0,0 +1,32 @@
+package science.wookup.emojioverload;
+
+public class FriendlyMessage {
+
+    private String text;
+    private String name;
+
+    public FriendlyMessage() {
+    }
+
+    public FriendlyMessage(String text, String name, String photoUrl) {
+        this.text = text;
+        this.name = name;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
